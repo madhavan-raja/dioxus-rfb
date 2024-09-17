af9427cdd03f59ec0ad111b786161f4a8f3a5504
Madhavan Raja <madhavanraja99@gmail.com>
2024-09-17 13:06:36 -0700

    Initial (and somewhat final) commit

---
+++ scripts/ground.js
@@ -0,0 +1,14 @@
+function Ground() {
+  this.x = 0;
+  var graphics = loadImage("assets/images/ground.png");
+
+  this.update = function () {
+    this.x += movementSpeed;
+
+    if (this.x <= -width) this.x = 0;
+  };
+
+  this.show = function () {
+    image(graphics, this.x, height - groundHeight);
+  };
+}
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+
+fn main() {
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        link { rel: "stylesheet", href: "styles/main.css" }
+        script { src: "scripts/p5.min.js" }
+        script { src: "scripts/sketch.js" }
+        script { src: "scripts/bird.js" }
+        script { src: "scripts/pipe.js" }
+        script { src: "scripts/ground.js" }
+    }
+}
